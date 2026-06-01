82c4044c06b17b11d418c95ef88e4a72c7a28c2a
Yashvardhan Kumar <yashram9798@gmail.com>
2026-06-02 02:07:26 +0530

    fix: correctly evaluate NEXT_PUBLIC_BASE_URL to prevent undefined in API requests

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,5 +1,7 @@
-const BASE_URL =
-  `${process.env.NEXT_PUBLIC_BASE_URL}/api` || 'http://localhost/api'
-const CODE_RUNNER_URL =
-  `${process.env.NEXT_PUBLIC_CODE_RUNNER_URL}` || 'http://10.82.221.23:2358'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+  ? `${process.env.NEXT_PUBLIC_BASE_URL}/api`
+  : 'http://localhost/api'
+const CODE_RUNNER_URL = process.env.NEXT_PUBLIC_CODE_RUNNER_URL
+  ? `${process.env.NEXT_PUBLIC_CODE_RUNNER_URL}`
+  : 'http://10.82.221.23:2358'
 export { BASE_URL, CODE_RUNNER_URL }
